5df8c3bd55f55af4e775630bc40659b7f9f28bda
Sam Sheets <sam.sheets94@gmail.com>
2021-09-16 14:10:36 -0600

    typeof, instanceof

---
+++ H2_typeofinstanceof/app.js
@@ -0,0 +1,27 @@
+var a = 3;
+console.log(typeof a); //number
+
+var b = "Hello";
+console.log(typeof b); //string
+
+var c = {};
+console.log(typeof c); //object
+
+var d = [];
+console.log(typeof d); // weird! -- object
+console.log(Object.prototype.toString.call(d)); // better! -- [object Array]
+
+function Person(name) {
+    this.name = name;
+}
+
+var e = new Person('Jane');
+console.log(typeof e); //object
+console.log(e instanceof Person); //true
+
+console.log(typeof undefined); // makes sense -- undefined
+console.log(typeof null); // a bug since, like, forever... -- object
+
+var z = function() { };
+console.log(typeof z); //function
+
